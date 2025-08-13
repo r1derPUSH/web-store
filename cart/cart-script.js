055c8fc2063b61850e640d8880ed690e59a2aca7
Maksym Kopak <riderworkable1234@gmail.com>
2025-08-13 12:17:54 +0300

    optimised localstorage

--- cart/cart-script.js
+++ cart/cart-script.js
@@ -14,6 +14,7 @@ btnBack.addEventListener('click', function () {
 
 document.querySelector('.clearStorage').addEventListener('click', function () {
     localStorage.clear();
+    window.location = window.location;
 })
 
 // fetching info
@@ -29,17 +30,66 @@ const arrOfJewelery = data.filter(item => item.category == "jewelery");
 // console.log(userCart);
 // products of user
 
-const t = localStorage.getItem('idOfMenClothing');
-console.log(JSON.parse(t));
+const manSectionLength = JSON.parse(localStorage.getItem('idOfMenClothing'));
+console.log(manSectionLength);
 
 class CartOfManClothing {
    // ІДЕЯ: ПРИВЯЗАТИ ПОДІЮ ПРИ ДОДАВАННІ АЙДІ ДО 'id' ТАКОЖ ПРИВЯЗАТИ НОВИЙ АЙТЕМ ЛОКАЛ СТОРЕДЖУ (НАПРИКЛАД idOfWomenCLothes) 
    // ДЛЯ ПОДАЛЬШОГО ВИКОРИСТАННЯ НА НАСТУПНІЙ СТОРІНЦІ ТА ДИНАМІЧНОГО ВІДОБРАЖЕННЯ ЦИХ ПРОДУКТІВ
-    
+    renderManCartSection () {
+        // container
+        for (let i = 0; i < manSectionLength.length; i++) {
+            // console.log(arrOfMenClothing[manSectionLength[i] - 1])
+            const container = document.createElement('div');
+            menC.appendChild(container);
+            container.style.display = 'flex';
+            container.style.justifyContent = 'space-around';
+            container.style.alignItems = 'center';
+            container.style.height = '12vh';
+            container.style.width = '100vw';
+            container.style.background = 'linear-gradient(135deg, #34495e, #5dade2)';
+            container.style.margin = '1vh';
+            // item name
+            const name = document.createElement('p');
+            container.appendChild(name);
+            name.style.fontSize = '1.2rem';
+            name.style.fontWeight = 'bold';
+            name.textContent = arrOfMenClothing[manSectionLength[i] - 1].title;
+            // img 
+            const img = document.createElement('img');
+            container.appendChild(img);
+            img.src = arrOfMenClothing[manSectionLength[i] - 1].image;
+            img.style.width = '8vh';
+            img.style.height = '8vh';
+            // price
+            const price = document.createElement('p');
+            container.appendChild(price);
+            price.textContent = arrOfMenClothing[manSectionLength[i] - 1].price + "$";
+            price.style.fontSize = '1.5rem';
+            price.style.fontWeight = 'bold';
+            //description
+            const description = document.createElement('p');
+            container.appendChild(description);
+            description.textContent = arrOfMenClothing[manSectionLength[i] - 1].description;
+            description.style.maxWidth = '20vw';
+            description.style.fontSize = '1.1rem';
+            description.style.overflow = 'hidden';
+            description.style.textOverflow = 'ellipsis';
+            description.style.display = '-webkit-box';
+            description.style.webkitLineClamp = '3';
+            description.style.webkitBoxOrient = 'vertical';
+            // quantity 
+            const quantity = document.createElement('p');
+            container.appendChild(quantity);
+            quantity.textContent = 'Quantity: 1';
+            quantity.style.fontSize = '1.3rem';
+            quantity.style.fontWeight = 'bold';
+        }
+    }
 }
 
 const createManProps = new CartOfManClothing();
-// createManProps.renderCartOfManClothing();
+createManProps.renderManCartSection();
 
 }
 
